aa31d2dd429dce0f3bdf5d9e4e9ad757140aebe0
bikokwa <peterbikokwa@gmail.com>
2018-02-05 00:15:56 +0300

    creating models 05/02/2018

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,15 @@
+#Each model class subclasses django.db.models.Model
 from django.db import models
 
-# Create your models here.
+#model class for question. It has two variables: question_text and publication_date
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    publication_date = models.DateTimeField('date published')
+
+#model class for Choice. It has two variables: choice_text and votes
+#each choice is related to a question
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length = 200)
+    votes = models.IntegerField(default = 0)
+
