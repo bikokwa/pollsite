41fd7956b7392bc8c053d540ddb9955c7ab863dd
bikokwa <peterbikokwa@gmail.com>
2018-02-05 20:08:55 +0300

    playing with API and creating admin 05/02/2018

--- polls/models.py
+++ polls/models.py
@@ -1,10 +1,22 @@
 #Each model class subclasses django.db.models.Model
 from django.db import models
 
+#import Python's datetime module
+import datetime
+
+#import django's timezone utilities
+from django.utils import timezone
+
 #model class for question. It has two variables: question_text and publication_date
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     publication_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.publication_date >= timezone.now() - datetime.timedelta(days=1)
 
 #model class for Choice. It has two variables: choice_text and votes
 #each choice is related to a question
@@ -12,4 +24,7 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length = 200)
     votes = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return self.choice_text
 
