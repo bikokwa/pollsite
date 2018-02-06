9944de43268ba13c1568336d371a53f7a5a95e5b
bikokwa <peterbikokwa@gmail.com>
2018-02-06 20:37:51 +0300

    django polls app part 3 completion 06/02/2018

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+#name of the app
+app_name = 'polls'
+
 urlpatterns = [
     path('', views.index, name = "index"),
+    path('<int:question_id>/details/', views.details, name = "details"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,15 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from . models import Question
 
 #display a message to the world
 def index(request):
-    return HttpResponse("Hello world. This is a polls site.")
+    latest_question_list = Question.objects.order_by('publication_date')[:5]
+    template = 'polls/index.html'
+    context = {
+        'latest_question_list':latest_question_list
+    }
+    return render(request, template, context)
+
+def details(request, question_id):
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request, 'polls/details.html', {'question':question})
