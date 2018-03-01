1857110ca172a4fb13655f755b011bb066f92cd1
bikokwa <peterbikokwa@gmail.com>
2018-03-01 22:34:22 +0300

    pollsite on 01/03/2018

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,36 @@
 from django.contrib import admin
 from . models import Question, Choice
 
-# tell admin that Question objects have an admin interface
-admin.site.register(Question)
-admin.site.register(Choice)
+#create the ability to add a bunch of Choices
+#with every Question added.
+
+#Choice objects are edited in the Question admin page.
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+# create a model admin class, and pass it as the second argument
+#to admin.site.register(Question) each time you need to change
+#the admin options for a model
+
+class QuestionAdminClass(admin.ModelAdmin):
+    #splitting the fields in fieldsets
+    fieldsets = [
+        ('Question to publish', {'fields':['question_text']}),
+        ('Date information', {'fields':['publication_date'], 'classes':['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+    #we can display individual fields using list_display as a tuple of columns
+    #to display on the change list page.
+    list_display = ('question_text','publication_date','was_published_recently')
+
+    #filters using the list_filter
+    #adds a FILTER sidebar that lets people filter by publication_date
+    list_filter = ['publication_date']
+
+    #let us add some search capability
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdminClass)
--- polls/models.py
+++ polls/models.py
@@ -18,6 +18,9 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.publication_date <= now
+    was_published_recently.admin_order_field = 'publication_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 #model class for Choice. It has two variables: choice_text and votes
 #each choice is related to a question
